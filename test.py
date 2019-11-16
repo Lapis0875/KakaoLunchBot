20798d927a10e7464c76587523de96f5a973ac55
Lapis0875 <lapis0875@gmail.com>
2019-11-17 02:14:56 +0900

    Update app.py

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ import json
 f = open('server/requesttemplate.json', 'rt', encoding='UTF8')
 data_str = f.read()
 data = json.loads(data_str)
-time = data['action']['params']['date']
-rd = json.loads(time.replace('/', ''))
+time = data['action']['detailParams']['date']['value']
+print(f'time : {time}')
 
 # 학교 급식 api 메뉴 사이트에서 급식메뉴 json 파일을 크롤링해온다. 해당 사이트 프로젝트 : https://github.com/5d-jh/school-menu-api
 school_type = 'high'
 school_code = 'B100000505'
-year, month, date = rd['value'].split('-')
+year, month, date = time.split('-')
 URL = f'https://schoolmenukr.ml/api/{school_type}/{school_code}?year={year}&month={month}&date={date}'
 
 result = json.loads(urlopen(URL).read().decode('utf-8'))
