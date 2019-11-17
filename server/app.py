8b644ce32df33835aab2b146ee0553b488af9743
Lapis0875 <lapis0875@gmail.com>
2019-11-17 10:47:11 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -22,12 +22,15 @@ def lunchmenu():
     requestdata.write(str(data))
     requestdata.close()
     print(f'data["bot"]["name"] = {data["bot"]["name"]}')
-    if data['bot']['name'] is '영훈고챗봇':
+    if data['bot']['name'] == '영훈고챗봇':
         print('유저가 챗봇을 통해 요청을 보냈음.')
         time = data['action']['detailParams']['date']['origin']
-    else:
+    elif data['bot']['name'] == '봇 이름':
         print('테스트 서버 응답으로 확인함.')
         time = data['action']['params']['date']
+    else:
+        response = {'version': '1.0', 'template': {'outputs': [{'simpleText': {'text': '잘못된 요청입니다. 만약 일반 사용자가 이러한 문구를 보고 있다면, 개발자가 오류를 해결할 수 있도록 알려주세요. '}}]}}
+        return response
     print(f'time : {time}')
 
     # 학교 급식 api 메뉴 사이트에서 급식메뉴 json 파일을 크롤링해온다. 해당 사이트 프로젝트 : https://github.com/5d-jh/school-menu-api
